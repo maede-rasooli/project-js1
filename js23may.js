298e4063969e40dc8c8c690176ac7dd918910569
maede-rasooli <161857545+maede-rasooli@users.noreply.github.com>
2024-05-22 10:21:39 -0700

    my files uploaded

---
+++ js23may.js
@@ -0,0 +1,14 @@
+let city = 'Herat';
+region = 'Bekrabad';
+populationdensity = 1200;
+area = 'Herat';
+console.log(city)
+console.log(region)
+console.log(populationdensity)
+console.log(area)
+
+var iscapital = true;
+console.log(typeof Boolean)
+
+var officiallanguage = ''
+console.log(typeof null)
